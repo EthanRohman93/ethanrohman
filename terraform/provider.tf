b03a2c5ad97ce4dcca2765c2bad65b3535165526
EthanRohman93 <ethanmrohman@gmail.com>
2025-05-16 18:52:34 -0500

    update

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,17 +1,3 @@
-
 provider "aws" {
     region = var.region
-}
-
-data "aws_ami" "amazon_linux" {
-  most_recent = true
-  owners      = ["amazon"]
-  filter {
-    name   = "name"
-    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
-  }
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
 }
