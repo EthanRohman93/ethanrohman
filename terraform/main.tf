5d1a745ee1ebf2a640408268cdb24b9b1ba1b272
EthanRohman93 <ethanmrohman@gmail.com>
2025-05-23 16:11:47 -0500

    come on baby

--- terraform/main.tf
+++ terraform/main.tf
@@ -42,23 +42,34 @@ resource "aws_instance" "ethanrohman" {
 
   user_data = <<-EOF
               #!/bin/bash
-              if command -v docker &> /dev/null
-              then
-                echo "Docker version: $(docker --version)"
-              else
-                echo "Docker not installed"
-              fi
-              
-              if command -v docker compose &> /dev/null
-              then
-                echo "Docker Compose version: $(docker-compose --version)"
-              else
-                echo "Docker Compose not installed"
-              fi
-              sudo apt update -y
-              sudo apt install nginx -y
-              sudo systemctl start nginx
-              sudo systemctlenable nginx
+              for pkg in docker.io docker-doc docker-compose docker-compose-v2 podman-docker containerd runc; do sudo apt-get remove $pkg; done
+              # Add Docker's official GPG key:
+              sudo apt-get update
+              sudo apt-get install ca-certificates curl
+              sudo install -m 0755 -d /etc/apt/keyrings
+              sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o
+              /etc/apt/keyrings/docker.asc
+              sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+              echo \
+                "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc]
+                https://download.docker.com/linux/ubuntu \
+                $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}") stable" | \
+                sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+              sudo apt-get update
+
+              sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+              sudo systemctl enable docker
+              sudo systemctl start docker
+              cd /home/ubuntu
+              git clone git clone https://github.com/${var.github_repo}
+
+              mkdir -p .certs
+              echo "${var.ethanrohman_crt}" > .certs/ethanrohman.com.crt
+              echo "${var.ethanrohman_key}" > .certs/ethanrohman.com.key
+              echo "${var.ethanrohman_bundle}" > .certs/ethanrohman.com.bundle
+
+              sudo docker compose up -d
               EOF
 
   tags = {
