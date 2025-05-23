ded7dd7e416e2888587d0ba6d515f59250a1504e
EthanRohman93 <ethanmrohman@gmail.com>
2025-05-23 16:24:22 -0500

    update

--- terraform/main.tf
+++ terraform/main.tf
@@ -51,18 +51,13 @@ resource "aws_instance" "ethanrohman" {
               /etc/apt/keyrings/docker.asc
               sudo chmod a+r /etc/apt/keyrings/docker.asc
 
-              echo \
-                "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc]
-                https://download.docker.com/linux/ubuntu \
-                \$({ echo \\\$((. /etc/os-release && echo \${UBUNTU_CODENAME:-\\$VERSION_CODENAME}))}) stable" | \
-                sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
               sudo apt-get update
 
               sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
               sudo systemctl enable docker
               sudo systemctl start docker
               cd /home/ubuntu
-              git clone git clone https://github.com/${var.github_repo}
+              git clone https://github.com/${var.github_repo}
 
               mkdir -p .certs
               echo "${var.ethanrohman_crt}" > .certs/ethanrohman.com.crt
