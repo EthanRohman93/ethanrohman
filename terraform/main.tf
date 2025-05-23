0bc082c3624caf41714561886e696e7e65e256bc
EthanRohman93 <ethanmrohman@gmail.com>
2025-05-23 16:22:04 -0500

    update

--- terraform/main.tf
+++ terraform/main.tf
@@ -54,7 +54,7 @@ resource "aws_instance" "ethanrohman" {
               echo \
                 "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc]
                 https://download.docker.com/linux/ubuntu \
-                $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}") stable" | \
+                \$({ echo \\\$((. /etc/os-release && echo \${UBUNTU_CODENAME:-\\$VERSION_CODENAME}))}) stable" | \
                 sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
               sudo apt-get update
 
