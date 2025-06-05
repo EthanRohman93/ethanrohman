817f4b0ee55f030255090a09c450539ebf4adbf8
EthanRohman93 <ethanmrohman@gmail.com>
2025-06-05 07:25:21 -0500

    update

--- terraform/setup.sh
+++ terraform/setup.sh
@@ -5,7 +5,7 @@
 su - ubuntu
 sudo apt update
 cd /home/ubuntu
-echo "$(ls)" > beast.txt
+echo "$(pwd)" > pwd_beast.txt
 # sudo apt install apt-transport-https ca-certificates curl software-properties-common
 # curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 # sudo apt update
