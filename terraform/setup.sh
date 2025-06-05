a3f7995ddb0c65e58997797d8b8f3516ce61bd77
EthanRohman93 <ethanmrohman@gmail.com>
2025-06-04 23:17:46 -0500

    come on baby

--- terraform/setup.sh
+++ terraform/setup.sh
@@ -27,6 +27,7 @@ git clone https://github.com/EthanRohman93/ethanrohman.git
 
 # aws install and ssl secrets
 
+sudo apt install unzip
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
 unzip awscliv2.zip
 sudo ./aws/install
