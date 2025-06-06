e0deaa9bb91cf14bce400f8878878c42f2570947
EthanRohman93 <ethanmrohman@gmail.com>
2025-06-05 20:54:40 -0500

    update

--- terraform/setup.sh
+++ terraform/setup.sh
@@ -15,7 +15,7 @@ sudo apt update
 
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}") stable" |  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt update 
-sudo apt install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo apt -y install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 # apt-cache policy docker-ce
 # sudo apt install docker-ce
 # sudo systemctl status docker
